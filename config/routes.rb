24e27f365633845fa5220f0c25f80d9f640fa5e1
Zachary Cohen <zachary@zacharyc.com>
2025-11-01 15:56:12 -0400

    Setting up Basic Authentication
    This is created with the rails generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # get "dashboard/show"
+  # get "home/index"
+  resource :session
+  resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,4 +15,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "home/index", as: :home
+  get "dashboard/show", as: :dashboard
+  root "home#index"
 end
